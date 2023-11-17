1532aa8a1d66873593bc0bde6a4c4882a395c816
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-17 17:25:46 +0000

    completed day 1 part 2

--- src/pages/2022/01.tsx
+++ src/pages/2022/01.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function puzzle() {
   const [part1, setPart1] = useState(0);
+  const [part2, setPart2] = useState(0);
   const inputFile = api.file.getInputFile.useQuery({ year: 2022, day: 1 });
   const data = inputFile.data;
 
@@ -20,7 +21,29 @@ export default function puzzle() {
           calories[elfIndex] += Number(row);
         }
       });
-      setPart1(Math.max(...calories));
+      const maxCalories = Math.max(...calories);
+      setPart1(maxCalories);
+
+      const sortedCalories = calories
+        .sort(function (a, b) {
+          return a - b;
+        })
+        .reverse();
+      const top3Calories: number[] = sortedCalories.slice(0, 3);
+      console.log(
+        "top 3 elves: " +
+          top3Calories[0] +
+          ", " +
+          top3Calories[1] +
+          ", " +
+          top3Calories[2],
+      );
+      const top3CaloriesSum: number = top3Calories.reduce(
+        (sum, a) => sum + a,
+        0,
+      );
+
+      setPart2(top3CaloriesSum);
     }
   };
 
@@ -37,6 +60,7 @@ export default function puzzle() {
         Get Results
       </button>
       <div>Part 1: {part1}</div>
+      <div>Part 2: {part2}</div>
     </div>
   );
 }
