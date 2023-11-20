f8e3a48ba23b774b10602d446daa90613d6b4f00
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-20 14:00:14 +0000

    day 2 part 2

--- src/pages/2022/02.tsx
+++ src/pages/2022/02.tsx
@@ -9,30 +9,104 @@ export default function Day02() {
 
   const processData = () => {
     if (data) {
-      let score: number = 0;
+      let part1Score: number = 0;
+      let part2Score: number = 0;
+      //   let exampleData = ["A Y", "B X", "C Z"];
       data.forEach((row: string) => {
         let splitRow = row.split(" ");
         // A Rock, B Paper, C Scissors
         // X Rock, Y Paper, Z Scissors
 
         if (splitRow.length == 2) {
+          // part 1
           switch (splitRow[1]) {
             case "X":
-              score += 1;
-              score += GetWinScore(splitRow[0], "C", "A");
+              part1Score += 1;
+              part1Score += GetWinScore(splitRow[0], "C", "A");
               break;
             case "Y":
-              score += 2;
-              score += GetWinScore(splitRow[0], "A", "B");
+              part1Score += 2;
+              part1Score += GetWinScore(splitRow[0], "A", "B");
               break;
             case "Z":
-              score += 3;
-              score += GetWinScore(splitRow[0], "B", "C");
+              part1Score += 3;
+              part1Score += GetWinScore(splitRow[0], "B", "C");
+              break;
+          }
+
+          // part 2
+          // X lose, Y draw, Z win
+          switch (splitRow[1]) {
+            case "X":
+              // lose
+              part2Score += 0;
+
+              switch (splitRow[0]) {
+                case "A":
+                  // rock
+                  // means I have to play scissors
+                  part2Score += 3;
+                  break;
+                case "B":
+                  // paper
+                  // means I have to play rock
+                  part2Score += 1;
+                  break;
+                case "C":
+                  // scissors
+                  // means I have to play paper
+                  part2Score += 2;
+                  break;
+              }
+              break;
+            case "Y":
+              // draw
+              part2Score += 3;
+              switch (splitRow[0]) {
+                case "A":
+                  // rock
+                  // means I have to play rock
+                  part2Score += 1;
+                  break;
+                case "B":
+                  // paper
+                  // means I have to play paper
+                  part2Score += 2;
+                  break;
+                case "C":
+                  // scissors
+                  // means I have to play scissors
+                  part2Score += 3;
+                  break;
+              }
+              break;
+            case "Z":
+              // win
+              part2Score += 6;
+              switch (splitRow[0]) {
+                case "A":
+                  // rock
+                  // means I have to play paper
+                  part2Score += 2;
+                  break;
+                case "B":
+                  // paper
+                  // means I have to play scissors
+                  part2Score += 3;
+                  break;
+                case "C":
+                  // scissors
+                  // means I have to play rock
+                  part2Score += 1;
+                  break;
+              }
               break;
           }
         }
       });
-      setPart1(score);
+
+      setPart1(part1Score);
+      setPart2(part2Score);
     }
   };
 
