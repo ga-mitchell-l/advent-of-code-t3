2bce4b5f79015d9366cbb4f2adef80f32ea0eb14
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-20 09:59:32 +0000

    tidied day 1 code

--- src/pages/2022/01.tsx
+++ src/pages/2022/01.tsx
@@ -1,43 +1,33 @@
 import { api } from "~/utils/api";
 import { useState } from "react";
 
-export default function puzzle() {
+export default function Day01() {
   const [part1, setPart1] = useState(0);
   const [part2, setPart2] = useState(0);
   const inputFile = api.file.getInputFile.useQuery({ year: 2022, day: 1 });
   const data = inputFile.data;
 
   const processData = () => {
-    console.log("processing data");
     if (data) {
       const numOfElves: number = data.filter((x) => x === "").length + 1;
-      console.log("numberOfElves " + numOfElves);
       let calories: number[] = Array(numOfElves).fill(0);
       let elfIndex: number = 0;
-      data.forEach((row) => {
+      data.forEach((row: string) => {
         if (row === "") {
           elfIndex++;
         } else {
           calories[elfIndex] += Number(row);
         }
       });
-      const maxCalories = Math.max(...calories);
+      const maxCalories: number = Math.max(...calories);
       setPart1(maxCalories);
 
-      const sortedCalories = calories
+      const sortedCalories: number[] = calories
         .sort(function (a, b) {
           return a - b;
         })
         .reverse();
       const top3Calories: number[] = sortedCalories.slice(0, 3);
-      console.log(
-        "top 3 elves: " +
-          top3Calories[0] +
-          ", " +
-          top3Calories[1] +
-          ", " +
-          top3Calories[2],
-      );
       const top3CaloriesSum: number = top3Calories.reduce(
         (sum, a) => sum + a,
         0,
