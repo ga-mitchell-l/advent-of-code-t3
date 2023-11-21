0ddc0266dfc0d855ba42dd7aa39efd652d3a129c
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-21 10:28:34 +0000

    example and input data

--- src/pages/2022/04.tsx
+++ src/pages/2022/04.tsx
@@ -6,7 +6,14 @@ export default function Day04() {
   const [part1, setPart1] = useState(0);
   const [part2, setPart2] = useState(0);
   const data = api.file.getInputFile.useQuery({ year: 2022, day: 4 }).data;
-  const exampleData: string[] = [];
+  const exampleData: string[] = [
+    "2-4,6-8",
+    "2-3,4-5",
+    "5-7,7-9",
+    "2-8,3-7",
+    "6-6,4-6",
+    "2-6,4-8",
+  ];
 
   const processData = () => {
     if (data) {
