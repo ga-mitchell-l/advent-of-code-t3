fd0efbc84c4db62649f02d9efc482a41d004166f
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-24 22:04:29 +0000

    tidied up part 1

--- src/pages/2022/09.tsx
+++ src/pages/2022/09.tsx
@@ -41,44 +41,11 @@ export default function Day05() {
         let distance = Number(distanceString);
 
         for (let d = 1; d <= distance; d++) {
-          let lastHPosition = hPosition.slice()[hPosition.length - 1];
-          let lastTPosition = tPosition.slice()[tPosition.length - 1];
-          let nextHPosition = Object.assign({}, lastHPosition);
-          let nextTPosition = Object.assign({}, lastTPosition);
+          const lastHPosition = hPosition[hPosition.length - 1];
+          const lastTPosition = tPosition[tPosition.length - 1];
 
-          if (direction === "U") {
-            nextHPosition.j++;
-          } else if (direction == "D") {
-            nextHPosition.j--;
-          } else if (direction == "L") {
-            nextHPosition.i--;
-          } else if (direction == "R") {
-            nextHPosition.i++;
-          }
-
-          // the tail always moves one step diagonally to keep up
-          let iTheSame = nextHPosition.i === lastTPosition.i;
-          let jTheSame = nextHPosition.j === lastTPosition.j;
-          let iDiff = nextHPosition.i - lastTPosition.i;
-          let jDiff = nextHPosition.j - lastTPosition.j;
-
-          if (!iTheSame && !jTheSame) {
-            if (Math.abs(jDiff) > 1 || Math.abs(iDiff) > 1) {
-              // move diagonally
-              nextTPosition.i += 1 * Math.sign(iDiff);
-              nextTPosition.j += 1 * Math.sign(jDiff);
-            }
-          } else if (!iTheSame) {
-            // moved horizontally
-            if (Math.abs(iDiff) > 1) {
-              nextTPosition.i += 1 * Math.sign(iDiff);
-            }
-          } else if (!jTheSame) {
-            // moved vertically
-            if (Math.abs(jDiff) > 1) {
-              nextTPosition.j += 1 * Math.sign(jDiff);
-            }
-          }
+          let nextHPosition = getNextHPosition(lastHPosition, direction);
+          let nextTPosition = getNextTPosition(lastTPosition, nextHPosition);
 
           hPosition.push(nextHPosition);
           tPosition.push(nextTPosition);
@@ -107,4 +74,53 @@ export default function Day05() {
       results={parts}
     ></Puzzle>
   );
+
+  function getNextHPosition(
+    lastHPosition: { i: number; j: number },
+    direction: string,
+  ) {
+    let nextHPosition = Object.assign({}, lastHPosition);
+
+    if (direction === "U") {
+      nextHPosition.j++;
+    } else if (direction == "D") {
+      nextHPosition.j--;
+    } else if (direction == "L") {
+      nextHPosition.i--;
+    } else if (direction == "R") {
+      nextHPosition.i++;
+    }
+    return nextHPosition;
+  }
+
+  function getNextTPosition(
+    lastTPosition: { i: number; j: number },
+    nextHPosition: { i: number; j: number },
+  ) {
+    let nextTPosition = Object.assign({}, lastTPosition);
+
+    let iTheSame = nextHPosition.i === lastTPosition.i;
+    let jTheSame = nextHPosition.j === lastTPosition.j;
+    let iDiff = nextHPosition.i - lastTPosition.i;
+    let jDiff = nextHPosition.j - lastTPosition.j;
+
+    if (!iTheSame && !jTheSame) {
+      if (Math.abs(jDiff) > 1 || Math.abs(iDiff) > 1) {
+        // move diagonally
+        nextTPosition.i += 1 * Math.sign(iDiff);
+        nextTPosition.j += 1 * Math.sign(jDiff);
+      }
+    } else if (!iTheSame) {
+      // moved horizontally
+      if (Math.abs(iDiff) > 1) {
+        nextTPosition.i += 1 * Math.sign(iDiff);
+      }
+    } else if (!jTheSame) {
+      // moved vertically
+      if (Math.abs(jDiff) > 1) {
+        nextTPosition.j += 1 * Math.sign(jDiff);
+      }
+    }
+    return nextTPosition;
+  }
 }
