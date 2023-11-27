f46b8eeb2aad5aeb983e3ced68be7d7016166124
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-27 17:43:10 +0000

    10 part 1

--- src/pages/2022/10.tsx
+++ src/pages/2022/10.tsx
@@ -22,13 +22,52 @@ export default function Day10() {
 
   const processData = (data: string[] | undefined) => {
     if (data) {
-      let part1Count: number = 0;
-      let part2Count: number = 0;
+      let cycle = 1;
+      let x = 1;
+      let inputLineNumber = 0;
+      let previousCmd = "";
+      let signalStrength = {};
 
-      data.forEach((row: string) => {});
+      while (data[inputLineNumber]) {
+        console.log("_____________");
+        signalStrength[cycle] = x * cycle;
+
+        console.log(data[inputLineNumber]);
+        if (previousCmd) {
+          console.log("previous cmd: " + previousCmd);
+          // set previous command to nothing
+          inputLineNumber++;
+          const [_, numerator] = previousCmd.split(" ");
+          x += Number(numerator);
+          previousCmd = "";
+        } else {
+          console.log("no previous cmd");
+          const [command, _] = data[inputLineNumber].split(" ");
+          if (command == "noop") {
+            inputLineNumber++;
+          } else if (command == "addx") {
+            previousCmd = data[inputLineNumber];
+          }
+        }
+        // signalStrength[cycle] = x * cycle;
+        console.log("cycle: " + cycle);
+        console.log("x: " + x);
+        console.log("signal strength: " + signalStrength[cycle]);
+
+        cycle++;
+      }
+
+      const signalStrengthSum =
+        signalStrength[20] +
+        signalStrength[60] +
+        signalStrength[100] +
+        signalStrength[140] +
+        signalStrength[180] +
+        signalStrength[220];
+
       setParts({
-        part1: part1Count,
-        part2: part2Count,
+        part1: signalStrengthSum,
+        part2: 0,
       });
     }
   };
