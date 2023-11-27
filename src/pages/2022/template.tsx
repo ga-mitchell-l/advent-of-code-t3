0a44889bc3359a87e401c4bd09c2572e17569b6a
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-27 17:43:52 +0000

    improved template

--- src/pages/2022/template.tsx
+++ src/pages/2022/template.tsx
@@ -3,34 +3,36 @@ import { useState } from "react";
 import Puzzle from "~/components/Puzzle";
 import { PartResults } from "~/classes/PuzzleResults";
 
-export default function Day05() {
+export default function Day10() {
   const [parts, setParts] = useState<PartResults>({
     part1: 0,
     part2: 0,
   });
 
-  const day = 5;
-  const data = api.file.getInputFile.useQuery({ year: 2022, day: day }).data;
-  const exampleData: string[] = [];
+  const day = 10;
+  const data: string[] = api.file.getInputFile.useQuery({
+    year: 2022,
+    day: day,
+  }).data;
+  const exampleData: string[] = api.file.getInputFile.useQuery({
+    year: 2022,
+    day: day,
+    example: true,
+  }).data;
 
   const processData = (data: string[] | undefined) => {
     if (data) {
-      let part1Count: number = 0;
-      let part2Count: number = 0;
-
-      data.forEach((row: string) => {});
       setParts({
-        part1: part1Count,
-        part2: part2Count,
+        part1: 0,
+        part2: 0,
       });
     }
   };
 
   return (
     <Puzzle
-      handleGetResults={() => {
-        processData(exampleData);
-      }}
+      handleGetResults={() => processData(data)}
+      handleExampleGetResults={() => processData(exampleData)}
       day={day}
       results={parts}
     ></Puzzle>
