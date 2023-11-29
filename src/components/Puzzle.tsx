9ca86e9635e5b93661337a35413d34027b2de9e5
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-29 08:37:08 +0000

    improved page look

--- src/components/Puzzle.tsx
+++ src/components/Puzzle.tsx
@@ -10,12 +10,12 @@ const Puzzle = ({
   results,
 }: PuzzleProps) => {
   return (
-    <div className="overflow-hidden bg-white py-5 font-mono sm:py-32">
+    <div className="overflow-hidden bg-white py-5 font-mono sm:py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
             <div className="lg:max-w-lg">
-              <h1 className="text-base font-semibold leading-7 text-indigo-600">
+              <h1 className="mb-2 text-2xl font-semibold leading-7 text-indigo-600">
                 Day {day}
               </h1>
               <Results
