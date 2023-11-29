e6d5fb66c4642d7b38ddd85278e393b13227428e
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-29 15:09:24 +0000

    get start and end position too

--- src/pages/2022/12-2.tsx
+++ src/pages/2022/12-2.tsx
@@ -21,15 +21,22 @@ export default function Day10() {
     "acctuvwj",
     "abdefghi",
   ];
+  const startChar = "S";
+  const endChar = "E";
+  const directions = [
+    [0, 1], // right
+    [0, -1], // left
+    [1, 0], // up
+    [-1, 0], // down
+  ];
 
   const processData = (data: string[] | undefined) => {
-    const start = "S";
-    const end = "E";
-
     if (data) {
-      let { elevation, visited } = ProcessInput(data);
+      let { elevation, visited, startPosition, endPosition } =
+        ProcessInput(data);
 
-      console.log(elevation);
+      console.log(startPosition);
+      console.log(endPosition);
       setParts({
         part1: 0,
         part2: 0,
@@ -49,20 +56,35 @@ export default function Day10() {
   function ProcessInput(data: string[]): processedData {
     let elevation: number[][] = [];
     let visited: boolean[][] = [];
+    let startPosition: number[];
+    let endPosition: number[];
+
     data.forEach((row) => {
+      let rowIndex = data.indexOf(row);
+      let startIndex = row.indexOf(startChar);
+      let endIndex = row.indexOf(endChar);
+
+      if (startIndex > -1) {
+        startPosition = [startIndex, rowIndex];
+      }
+      if (endIndex > -1) {
+        endPosition = [endIndex, rowIndex];
+      }
       let elevationRow = row
-        .replace("S", "a")
-        .replace("E", "z")
+        .replace(startChar, "a")
+        .replace(endChar, "z")
         .split("")
         .map((x) => Number(x.charCodeAt(0)) - "a".charCodeAt(0));
       elevation.push(elevationRow);
       visited.push(Array<boolean>(elevationRow.length).fill(false));
     });
-    return { elevation, visited };
+    return { elevation, visited, startPosition, endPosition };
   }
 
   interface processedData {
     elevation: number[][];
     visited: boolean[][];
+    startPosition: number[];
+    endPosition: number[];
   }
 }
