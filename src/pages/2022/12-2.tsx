c9aaa8aaf4d3a2e62cd82cf97e79001834c57696
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-11-29 14:56:06 +0000

    process data function

--- src/pages/2022/12-2.tsx
+++ src/pages/2022/12-2.tsx
@@ -23,7 +23,13 @@ export default function Day10() {
   ];
 
   const processData = (data: string[] | undefined) => {
+    const start = "S";
+    const end = "E";
+
     if (data) {
+      let { elevation, visited } = ProcessInput(data);
+
+      console.log(elevation);
       setParts({
         part1: 0,
         part2: 0,
@@ -39,4 +45,24 @@ export default function Day10() {
       results={parts}
     ></Puzzle>
   );
+
+  function ProcessInput(data: string[]): processedData {
+    let elevation: number[][] = [];
+    let visited: boolean[][] = [];
+    data.forEach((row) => {
+      let elevationRow = row
+        .replace("S", "a")
+        .replace("E", "z")
+        .split("")
+        .map((x) => Number(x.charCodeAt(0)) - "a".charCodeAt(0));
+      elevation.push(elevationRow);
+      visited.push(Array<boolean>(elevationRow.length).fill(false));
+    });
+    return { elevation, visited };
+  }
+
+  interface processedData {
+    elevation: number[][];
+    visited: boolean[][];
+  }
 }
