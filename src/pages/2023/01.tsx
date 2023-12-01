6968b77fa1a8b270d9450dd356f18ede2a24d761
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-01 09:15:23 +0000

    stubbed out 2023 day 1

---
+++ src/pages/2023/01.tsx
@@ -0,0 +1,41 @@
+import { api } from "~/utils/api";
+import { useState } from "react";
+import Puzzle from "~/components/Puzzle";
+import type { PartResults } from "~/classes/PuzzleResults";
+
+export default function Day01() {
+  const [parts, setParts] = useState<PartResults>({
+    part1: 0,
+    part2: 0,
+  });
+
+  const day = 1;
+  const data: string[] = api.file.getInputFile.useQuery({
+    year: 2023,
+    day: day,
+  }).data;
+  const exampleData: string[] = [
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet",
+  ];
+
+  const processData = (data: string[] | undefined) => {
+    if (data) {
+      setParts({
+        part1: 0,
+        part2: 0,
+      });
+    }
+  };
+
+  return (
+    <Puzzle
+      handleGetResults={() => processData(data)}
+      handleExampleGetResults={() => processData(exampleData)}
+      day={day}
+      results={parts}
+    ></Puzzle>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,6 +30,14 @@ export default function Home() {
               <h3 className="text-2xl font-bold">2022</h3>
             </Link>
           </div>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            <Link
+              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              href="2023/01"
+            >
+              <h3 className="text-2xl font-bold">2023</h3>
+            </Link>
+          </div>
         </div>
       </main>
     </>
