7147f7cea5002396b8c56bbdaafd2c086bbc8f19
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-03 09:38:59 +0000

    i've done something but it has not answered the question

--- src/pages/2023/03.tsx
+++ src/pages/2023/03.tsx
@@ -21,10 +21,68 @@ export default function Day03() {
     example: true,
   }).data;
 
+  const symbolRegex = /[*#+$%\-\@\/=]/g;
+
   const processData = (data: string[] | undefined) => {
+    // data = ["467..114..", "...*......", "..35..633."];
+    // data = ["467", ".*.", "..3"];
+    let part1 = 0;
+    const validNumbers: number[] = [];
     if (data) {
+      const schematic: string[][] = [];
+      data.forEach((row) => {
+        const splitRow = row.split("");
+        schematic.push(splitRow);
+      });
+
+      schematic.forEach((row) => {
+        console.log("--------------");
+        console.log("row: " + row);
+        const rowIndex = schematic.indexOf(row);
+
+        for (let chunkIndex = 0; chunkIndex < row.length; chunkIndex++) {
+          console.log("- - - - - -");
+          console.log("index: " + rowIndex + ", " + chunkIndex);
+          let chunk = row[chunkIndex];
+          let floop = "";
+          console.log("chunk: " + chunk);
+          const numberChunk = Number(chunk);
+          if (!isNaN(numberChunk)) {
+            for (
+              let adjRowIndex = rowIndex - 1;
+              adjRowIndex < rowIndex + 2;
+              adjRowIndex++
+            ) {
+              if (adjRowIndex > -1 && adjRowIndex < schematic.length) {
+                let adjRow = schematic[adjRowIndex];
+                console.log(adjRow);
+
+                for (
+                  let adjChunkIndex = chunkIndex - 1;
+                  adjChunkIndex < chunkIndex + 2;
+                  adjChunkIndex++
+                ) {
+                  if (adjChunkIndex > -1 && adjChunkIndex < adjRow.length) {
+                    let adjChunk = adjRow[adjChunkIndex];
+                    floop = floop + adjChunk;
+                  }
+                }
+              }
+            }
+            console.log("floop: " + floop);
+            const match = [...floop.matchAll(symbolRegex)];
+            if (match.length > 0) {
+              console.log("it's a match: " + chunk);
+              validNumbers.push(numberChunk);
+              console.log(validNumbers);
+            }
+          }
+        }
+
+        part1 = validNumbers.reduce((p, q) => p + q, 0);
+      });
       setParts({
-        part1: 0,
+        part1: part1,
         part2: 0,
       });
     }
