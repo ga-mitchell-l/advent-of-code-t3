ad7693e163ca13badc14f0afbf8586ff11390390
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-05 17:31:05 +0000

    part 2 start

--- src/pages/2023/05.tsx
+++ src/pages/2023/05.tsx
@@ -24,18 +24,24 @@ export default function Day05() {
 
   type AlmanacMap = {
     sourceCategory: string;
-    ranges: Range[];
+    ranges: RangeMap[];
   };
 
   type ReturnType = {
     seeds: number[];
     almanacMaps: AlmanacMap[];
+    seedRanges: Range[];
   };
 
-  type Range = {
+  type RangeMap = {
     souceRangeStart: number;
     sourceRangeEnd: number;
     destinationDiff: number;
+  };
+
+  type Range = {
+    start: number;
+    end: number;
   };
 
   function GetEmptyAlmanacMap() {
@@ -51,19 +57,24 @@ export default function Day05() {
       const results = ProcessInput(data);
 
       let source = results.seeds;
+      let part2Source = results.seedRanges;
       results.almanacMaps.forEach((map) => {
         source = MoveToDestination(source, map.ranges);
       });
-
-      console.log(results.almanacMaps);
-      console.log("seeds: " + results.seeds);
-      console.log("locations: " + source);
+      for (let i = 0; i < 2; i++) {
+        console.log(" - - - MAP - - - -");
+        const map = results.almanacMaps[i];
+        part2Source = RangeMoveToDestination(part2Source, map.ranges);
+        console.log(part2Source);
+      }
 
       const minLocation = Math.min(...source);
+      const part2Locations = part2Source.map((x) => x.start);
+      const minPart2Location = Math.min(...part2Locations);
 
       setParts({
         part1: minLocation,
-        part2: 0,
+        part2: minPart2Location,
       });
     }
   };
@@ -77,25 +88,88 @@ export default function Day05() {
     ></Puzzle>
   );
 
-  function MoveToDestination(seeds: number[], mapRanges: Range[]): number[] {
-    const soils: number[] = [];
-    seeds.forEach((seed) => {
+  function MoveToDestination(
+    sources: number[],
+    mapRanges: RangeMap[],
+  ): number[] {
+    const destinations: number[] = [];
+    sources.forEach((source) => {
       const mapsInRange = mapRanges.filter(
         (range) =>
-          range.souceRangeStart <= seed && seed <= range.sourceRangeEnd,
+          range.souceRangeStart <= source && source <= range.sourceRangeEnd,
       );
       if (mapsInRange.length == 1) {
-        const destination = mapsInRange[0].destinationDiff + seed;
-        soils.push(destination);
+        const destination = mapsInRange[0].destinationDiff + source;
+        destinations.push(destination);
       } else {
-        soils.push(seed);
+        destinations.push(source);
       }
     });
-    return soils;
+    return destinations;
+  }
+
+  function RangeMoveToDestination(
+    sources: Range[],
+    mapRanges: RangeMap[],
+  ): Range[] {
+    const destinations: Range[] = [];
+
+    console.log("sources: ");
+    console.log(sources);
+
+    while (sources.length > 0) {
+      console.log("-----------");
+      const source = sources.pop();
+      console.log("SOURCE");
+      console.log(source);
+      console.log("sources left: " + sources.length);
+
+      const start = source.start;
+      const end = source.end;
+      const mapsInRangeSource = mapRanges.filter(
+        (range) =>
+          range.souceRangeStart <= start && start <= range.sourceRangeEnd,
+      );
+      const mapsInRangeDestination = mapRanges.filter(
+        (range) => range.souceRangeStart <= end || end <= range.sourceRangeEnd,
+      );
+      if (mapsInRangeSource.length == 0 && mapsInRangeDestination.length == 0) {
+        // range is not included in any map
+        // map to itself
+        console.log("range not included in map");
+        const destination: Range = { start: start, end: end };
+        destinations.push(destination);
+        console.log(destinations);
+      } else if (mapsInRangeSource.length == 1) {
+        const map = mapsInRangeSource[0];
+        console.log(map.sourceRangeEnd);
+        if (end <= map.sourceRangeEnd) {
+          console.log("range totally included in map");
+          // range is included in map totally
+          const destination: Range = {
+            start: start + map.destinationDiff,
+            end: end + map.destinationDiff,
+          };
+          destinations.push(destination);
+          console.log(destinations);
+        } else {
+          console.log("range partially included in map");
+          // split up the range and do it again
+          const rangeLeft: Range = { start: start, end: map.sourceRangeEnd };
+          sources.push(rangeLeft);
+          const rangeRight: Range = { start: map.sourceRangeEnd + 1, end: end };
+          sources.push(rangeRight);
+          console.log("new number of sources: " + sources.length);
+        }
+      }
+    }
+
+    return destinations;
   }
 
   function ProcessInput(data: string[]): ReturnType {
     let seeds: number[] = [];
+    let seedRanges: Range[] = [];
     const almanacMaps: AlmanacMap[] = [];
     let currentAlmanacMap = GetEmptyAlmanacMap();
 
@@ -116,6 +190,14 @@ export default function Day05() {
         if (heading == "seeds") {
           const seedString = colonSplit[1];
           seeds = GetNumberArray(seedString);
+
+          for (let i = 0; i < seeds.length; i += 2) {
+            const range: Range = {
+              start: seeds[i],
+              end: seeds[i] + seeds[i + 1] - 1,
+            };
+            seedRanges.push(range);
+          }
           return;
         }
 
@@ -130,7 +212,7 @@ export default function Day05() {
       const [destinationRangeStart, sourceRangeStart, rangeLength] =
         GetNumberArray(row);
 
-      const range: Range = {
+      const range: RangeMap = {
         souceRangeStart: sourceRangeStart,
         sourceRangeEnd: sourceRangeStart + rangeLength - 1,
         destinationDiff: -sourceRangeStart + destinationRangeStart,
@@ -143,6 +225,9 @@ export default function Day05() {
       almanacMaps.push(currentAlmanacMap);
     }
 
-    return { seeds: seeds, almanacMaps: almanacMaps };
+    console.log(seeds);
+    console.log(seedRanges[0]);
+
+    return { seeds: seeds, almanacMaps: almanacMaps, seedRanges: seedRanges };
   }
 }
