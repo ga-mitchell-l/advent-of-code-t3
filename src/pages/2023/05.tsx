4e210bdbfe229b8ab40eae30e95d2011b792e292
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-05 10:56:31 +0000

    started processing input

--- src/pages/2023/05.tsx
+++ src/pages/2023/05.tsx
@@ -2,6 +2,7 @@ import { api } from "~/utils/api";
 import { useState } from "react";
 import Puzzle from "~/components/Puzzle";
 import type { PartResults } from "~/classes/PuzzleResults";
+import { GetNumberArray } from "@utils/react";
 
 export default function Day05() {
   const [parts, setParts] = useState<PartResults>({
@@ -21,8 +22,63 @@ export default function Day05() {
     example: true,
   }).data;
 
+  type AlmanacMap = {
+    sourceCategory: string;
+    destinationCategory: string;
+    sourceRange: number[];
+    destinationRange: number[];
+  };
+
+  function GetEmptyAlmanacMap() {
+    const map: AlmanacMap = {
+      sourceCategory: "",
+      destinationCategory: "",
+      sourceRange: [],
+      destinationRange: [],
+    };
+    return map;
+  }
+
   const processData = (data: string[] | undefined) => {
     if (data) {
+      let seeds: number[] = [];
+      const almanacMaps: AlmanacMap[] = [];
+      let currentAlmanacMap = GetEmptyAlmanacMap();
+
+      data.forEach((row) => {
+        console.log("-----");
+        console.log(row);
+        if (row == "") {
+          if (currentAlmanacMap.sourceCategory != "") {
+            almanacMaps.push(currentAlmanacMap);
+          }
+          currentAlmanacMap = GetEmptyAlmanacMap();
+          return;
+        }
+
+        const colonSplit = row.split(":");
+        if (colonSplit.length > 1) {
+          const heading = colonSplit[0];
+          if (heading == "seeds") {
+            const seedString = colonSplit[1];
+            seeds = GetNumberArray(seedString);
+            console.log(seeds);
+            return;
+          }
+
+          const currentSection = heading.split(" ")[0];
+          const [source, _, destination] = currentSection.split("-");
+          currentAlmanacMap.sourceCategory = source;
+          currentAlmanacMap.destinationCategory = destination;
+
+          return;
+        }
+
+        const [destinationRangeStart, sourceRangeStart, rangeLength] =
+          GetNumberArray(row);
+      });
+
+      console.log(almanacMaps);
       setParts({
         part1: 0,
         part2: 0,
