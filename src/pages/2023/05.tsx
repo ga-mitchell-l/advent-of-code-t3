5245e534f39f12ee165dd2c12dd6da6968836e91
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-06 11:53:55 +0000

    part 2 example working

--- src/pages/2023/05.tsx
+++ src/pages/2023/05.tsx
@@ -51,20 +51,29 @@ export default function Day05() {
     if (data) {
       const results = ProcessInput(data);
 
+      console.log("almanac map:");
+      console.log(results.almanacMaps);
+
       let source = results.seeds;
       let part2Source = results.seedRanges;
-      results.almanacMaps.forEach((map) => {
-        source = MoveToDestination(source, map.ranges);
-      });
-      for (let i = 0; i < 2; i++) {
-        console.log(" - - - MAP - - - -");
+      // results.almanacMaps.forEach((map) => {
+      //   source = MoveToDestination(source, map.ranges);
+      // });
+      for (let i = 0; i < results.almanacMaps.length; i++) {
         const map = results.almanacMaps[i];
+        console.log(" - - - MAP " + map.sourceCategory + " - - - -");
+        console.log("starting source");
+        part2Source.forEach((sauce) => console.log(sauce));
+        console.log("maps");
+        console.log(map);
         part2Source = RangeMoveToDestination(part2Source, map.ranges);
-        console.log(part2Source);
       }
 
+      console.log("ending output");
+      part2Source.forEach((sauce) => console.log(sauce));
+
       const minLocation = Math.min(...source);
-      const part2Locations = part2Source.map((x) => x.start);
+      const part2Locations = part2Source.map((x) => x[0]);
       const minPart2Location = Math.min(...part2Locations);
 
       setParts({
@@ -109,13 +118,12 @@ export default function Day05() {
   ): number[][] {
     const destinations: number[][] = [];
 
-    console.log("sources: ");
-    console.log(sources);
+    console.log();
 
     while (sources.length > 0) {
       console.log("-----------");
       const source = sources.pop();
-      console.log("SOURCE");
+      console.log("SOURCE popped:");
       console.log(source);
       console.log("sources left: " + sources.length);
 
@@ -126,18 +134,23 @@ export default function Day05() {
           range.souceRangeStart <= start && start <= range.sourceRangeEnd,
       );
       const mapsInRangeDestination = mapRanges.filter(
-        (range) => range.souceRangeStart <= end || end <= range.sourceRangeEnd,
+        (range) => range.souceRangeStart <= end && end <= range.sourceRangeEnd,
       );
+      console.log("maps in start: " + mapsInRangeSource.length);
+      console.log("maps in end: " + mapsInRangeDestination.length);
+      console.log(mapsInRangeDestination);
       if (mapsInRangeSource.length == 0 && mapsInRangeDestination.length == 0) {
         // range is not included in any map
         // map to itself
         console.log("range not included in map");
         const destination: number[] = [start, end];
         destinations.push(destination);
-        console.log(destinations);
+        console.log("updated destinations:");
+        destinations.forEach((dest) => console.log(dest));
       } else if (mapsInRangeSource.length == 1) {
         const map = mapsInRangeSource[0];
-        console.log(map.sourceRangeEnd);
+        console.log(map);
+        console.log("matched map range end: " + map.sourceRangeEnd);
         if (end <= map.sourceRangeEnd) {
           console.log("range totally included in map");
           // range is included in map totally
@@ -146,7 +159,8 @@ export default function Day05() {
             end + map.destinationDiff,
           ];
           destinations.push(destination);
-          console.log(destinations);
+          console.log("updated destinations:");
+          destinations.forEach((dest) => console.log(dest));
         } else {
           console.log("range partially included in map");
           // split up the range and do it again
@@ -154,8 +168,19 @@ export default function Day05() {
           sources.push(rangeLeft);
           const rangeRight: number[] = [map.sourceRangeEnd + 1, end];
           sources.push(rangeRight);
-          console.log("new number of sources: " + sources.length);
+          console.log("updated sources:");
+          sources.forEach((dest) => console.log(dest));
         }
+      } else if (mapsInRangeDestination.length == 1) {
+        const map = mapsInRangeDestination[0];
+        console.log(map);
+        console.log("match mapped range start:" + map.souceRangeStart);
+        const rangeLeft: number[] = [start, map.souceRangeStart - 1];
+        const rangeRight: number[] = [map.souceRangeStart, end];
+        sources.push(rangeLeft);
+        sources.push(rangeRight);
+        console.log("updated sources:");
+        sources.forEach((dest) => console.log(dest));
       }
     }
 
@@ -216,9 +241,6 @@ export default function Day05() {
     if (currentAlmanacMap.sourceCategory != "") {
       almanacMaps.push(currentAlmanacMap);
     }
-
-    console.log(seeds);
-    console.log(seedRanges[0]);
 
     return { seeds: seeds, almanacMaps: almanacMaps, seedRanges: seedRanges };
   }
