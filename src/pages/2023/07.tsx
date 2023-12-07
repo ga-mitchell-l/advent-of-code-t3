954e313ee3dba57b411c222c5b158440930b68be
Lizzy Mitchell <lizzy.mitchell@gembaadvantage.com>
2023-12-07 08:18:39 +0000

    stubbed out day 7

---
+++ src/pages/2023/07.tsx
@@ -0,0 +1,43 @@
+import { api } from "~/utils/api";
+import { useState } from "react";
+import Puzzle from "~/components/Puzzle";
+import type { PartResults } from "~/classes/PuzzleResults";
+
+export default function Day07() {
+  const [parts, setParts] = useState<PartResults>({
+    part1: 0,
+    part2: 0,
+  });
+
+  const day = 7;
+  const year = 2023;
+  const data: string[] = api.file.getInputFile.useQuery({
+    year: year,
+    day: day,
+  }).data;
+  const exampleData: string[] = [
+    "32T3K 765",
+    "T55J5 684",
+    "KK677 28",
+    "KTJJT 220",
+    "QQQJA 483",
+  ];
+
+  const processData = (data: string[] | undefined) => {
+    if (data) {
+      setParts({
+        part1: 0,
+        part2: 0,
+      });
+    }
+  };
+
+  return (
+    <Puzzle
+      handleGetResults={() => processData(data)}
+      handleExampleGetResults={() => processData(exampleData)}
+      day={day}
+      results={parts}
+    ></Puzzle>
+  );
+}
